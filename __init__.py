7a8356950c9efda5f3920802f93ffd5d43636730
Trinaya Kantevari <108997403+trinaya-kantevari@users.noreply.github.com>
2024-11-14 01:53:27 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,31 +23,60 @@ def main(mytimer: func.TimerRequest) -> None:
     identify_stale_snapshots()
 
 def identify_stale_snapshots():
+
     snapshots = compute_client.snapshots.list()
+    disks = compute_client.disks.list()
+    existing_disk_ids = {disk.id for disk in disks}
+    unattached_disk_ids = []
+    vm_list = compute_client.virtual_machines.list_all()
+    attached_disk_ids = set()
+    for vm in vm_list:
+        if vm.storage_profile.os_disk.managed_disk:
+            attached_disk_ids.add(vm.storage_profile.os_disk.managed_disk.id)
+    for data_disk in vm.storage_profile.data_disks:
+        if data_disk.managed_disk:
+            attached_disk_ids.add(data_disk.managed_disk.id)
+    for disk in disks:
+        if disk.id not in attached_disk_ids:
+            unattached_disk_ids.append(disk.id)
+
     for snapshot in snapshots:
-        logging.info(f"\n\n\n\n\nSnapshot Name: {snapshot.name}, Snapshot ID: {snapshot.id}\n\n\n\n\n\n")
         tags = snapshot.tags
         retention_period = int(tags['RetentionPeriod'])
-        created_by = tags['CreatedBy']
-        auto_delete = bool(tags['AutoDelete'])
+        created_by = tags['CreatedByEmail']
         creation_date = snapshot.time_created
         current_date = datetime.datetime.now(datetime.timezone.utc)
         resource_group_name = snapshot.id.split("/")[4]
         age_minutes = int((current_date - creation_date).total_seconds() / 60)
-        logging.info(f"\n\n\n\n\n\n\n\nProcessing snapshot: {snapshot.name},\n\n RG: {resource_group_name},\n\nAutoDelete: {auto_delete},\n\n "
-                             f"RetentionPeriod: {retention_period},\n\n CreatedBy: {created_by},\n\n "
-                             f"CreationDate: {creation_date}\n\n\n\n\n\n\n")
+        x = False
+        y = False
+
+        snapshot_disk_id = snapshot.creation_data.source_resource_id
+        if snapshot_disk_id not in existing_disk_ids:
+            logging.info(f"Snapshot {snapshot.name} corresponds to a deleted disk.")
+            x = True
+        if snapshot_disk_id in unattached_disk_ids:
+            logging.info(f"Snapshot {snapshot.name} corresponds to a deleted disk.")
+            y = True
+        inactive = x or y
+        
         if age_minutes > retention_period:
-            logging.info("\n\n\n\n\nDeleting Snapshot.... \n\n\n\n\n\n")
-            compute_client.snapshots.begin_delete(resource_group_name,snapshot_name=snapshot.name,)
-            logging.info(f"\n\n\n\n\nSnapshot has been DELETED {snapshot.name}, Snapshot ID: {snapshot.id}\n\n\n\n\n\n")
-            send_deletion_email(created_by, snapshot.name, creation_date, retention_period, age_minutes)
-        else:
-            notify_owner(created_by, snapshot.name, age_minutes)
-            logging.info("\nMAIL SENT AS NOTIFY !!\n\n\n\n\n\n")
+            if inactive:
+                delete_snapshots(resource_group_name,snapshot.name,created_by,creation_date,retention_period,age_minutes)
+                
+            else:
+                tags['RetentionPeriod'] = str(retention_period + 10)
 
+        if age_minutes > 30:
+            pass
+            notify(created_by, snapshot.name, creation_date, retention_period, age_minutes)
 
-    
+def delete_snapshots(resource_group_name,snapshotName,created_by,creation_date,retention_period,age_minutes):
+    logging.info(" \n\n\n\n\n  Deleting Snapshot.... \n\n\n\n\n\n ")
+    compute_client.snapshots.begin_delete(resource_group_name,snapshot_name=snapshotName,)
+    logging.info(f" \n\n\n\n\n  SNAPSHOT HAS BEEN DELETED {snapshotName} !! \n\n\n\n\n\n ")
+    send_deletion_email(created_by, snapshotName, creation_date, retention_period, age_minutes)
+
 def send_mail(created_by, email_body):
 
     sender_email = ' '
@@ -59,7 +88,8 @@ def send_mail(created_by, email_body):
     server.sendmail(sender_email, created_by, email_body)
     server.quit()
 
-    logging.info('\n\n\n\n\n\n\n\n\n\n\n MAIL HAS BEEN SENT !! !! !!\n\n\n\n\n\n\n\n\n\n\n')
+    logging.info(' \n\n\n\n\n\n\n\n\n\n\n   MAIL HAS BEEN SENT !! !! !!  \n\n\n\n\n\n\n\n\n\n\n ')
+
 
 def send_deletion_email(created_by, snapshot_name, creation_date, retention_period, age_minutes):
 
@@ -76,27 +106,28 @@ def send_deletion_email(created_by, snapshot_name, creation_date, retention_peri
     - Retention Period: {retention_period} minutes
     - Snapshot Age: {age_minutes} minutes
 
-    If you need to retrieve this snapshot, please contact us within 30 days from today.
-
     Best regards,
     Azure Team
     """
     send_mail(created_by, email_body)
 
 
-
-def notify_owner(created_by, snapshot_name, age_minutes):
+def notify(created_by, snapshot_name, creation_date, retention_period, age_minutes):
     email_body = f"""
-        Hello,
+    Hello,
 
-        The snapshot '{snapshot_name}' is now {age_minutes} minutes old. Please review it to ensure it is still needed. 
-        You can either delete the snapshot or update its tag values if necessary.
+    The snapshot '{snapshot_name}', created on {creation_date.strftime('%Y-%m-%d')}, is still available since 3 months.
+    Please check it and update it's retention period tag.
 
-        Snapshot Details:
-        - Name: {snapshot_name}
-        - Age: {age_minutes} minutes
 
-        Best regards,
-        Azure Team
-        """
+    Snapshot Details:
+    - Name: {snapshot_name}
+    - Creation Date: {creation_date.strftime('%Y-%m-%d')}
+    - Creation Time: {creation_date.strftime('%H:%M:%S')}
+    - Retention Period: {retention_period} minutes
+    - Snapshot Age: {age_minutes} minutes
+
+    Best regards,
+    Azure Team
+    """
     send_mail(created_by, email_body)
